72c71c845e360cbcada6bc58fbcd5027ac7d7f2f
Mohit <mohitsai.kallam@gmail.com>
2026-05-25 07:41:01 +0530

    commiting corectly

---
+++ 02-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh:hello ramesh how are you"
+echo "ramesh:i am fine how are your"
+echo "suresh: i am fine are you coming to so and so"
+echo "ramesh : yes i am"
---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+p1=suresh
+p2=ramnesh
+echo "$p1:hello ramesh how are you"
+echo "$p2:i am fine how are your"
+echo "$p1: i am fine are you coming to so and so"
+echo "$p2 : yes i am"
