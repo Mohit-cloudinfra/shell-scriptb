c8f243995f21a2df0cab5dc73a0f2921ab2556e1
Mohit <mohitsai.kallam@gmail.com>
2026-06-01 21:24:45 +0530

    conditions

---
+++ 09-conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read n
+# -gt greter than
+# -lt less than 
+# -eq equal
+# -ne not equal
+
+if [$n -gt 20]; then
+    echo "$n is greater"
+elif [$n -lt 20]; then
+    echo "$n is lesser"
+elif [$n -eq 20]; then
+    echo "equal"
+fi
+
