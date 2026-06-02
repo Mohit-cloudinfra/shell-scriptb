590730b869d80f4ff2ebdfb45605d6fac3810904
Mohit <mohitsai.kallam@gmail.com>
2026-06-02 09:01:50 +0530

    functions

---
+++ 12-log.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+logs="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+mkdir -p "/var/log/shell-script"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+echo "Installing Nginx"
+dnf install nginx -y &>> $logs
+VALIDATE $? "nginx installation"
+
+
+dnf install mysql -y & >> $logs
+VALIDATE $? "mysql installation"
+
+dnf install nodejs -y & >> $logs
+VALIDATE $? "nodejs installation"
