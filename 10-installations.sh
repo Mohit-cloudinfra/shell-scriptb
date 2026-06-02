3cf4c331105f2f6119e86bfd2e8a98df5a6fae7d
Mohit <mohitsai.kallam@gmail.com>
2026-06-02 06:45:03 +0530

    installations

--- 10-installations.sh
+++ 10-installations.sh
@@ -5,9 +5,10 @@ u=$(id -u)
 if [ $u -ne 0 ]; then
     echo "user is not root"
     exit 1
+fi
+dnf install ngindddx -y
 
-dnf install nginx -y
-
-if [ $? -ne 0 ]
+if [ $? -ne 0 ]; then
     echo "there is failure"
     exit 1
+fi
